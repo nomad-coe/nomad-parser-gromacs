5b08f3b93d34d8378f6d6f833e68d8a428b8be12
Berk Onat <b.onat@warwick.ac.uk>
2017-12-01 15:10:20 +0000

    Added tests to pipeline

--- src/test/scala/eu/nomad_lab/parsers/GromacsParserSpec.scala
+++ src/test/scala/eu/nomad_lab/parsers/GromacsParserSpec.scala
@@ -4,11 +4,29 @@ import org.specs2.mutable.Specification
 
 object GromacsParserTests extends Specification {
   "GromacsParserTest" >> {
-    "test with json-events" >> {
+    "[aminoacids] test with json-events" >> {
       ParserRun.parse(GromacsParser, "parsers/gromacs/test/examples/aminoacids/md.log", "json-events") must_== ParseResult.ParseSuccess
     }
-    "test with json" >> {
+    "[argon] test with json-events" >> {
+      ParserRun.parse(GromacsParser, "parsers/gromacs/test/examples/argon/md.log", "json-events") must_== ParseResult.ParseSuccess
+    }
+    "[water] test with json-events" >> {
+      ParserRun.parse(GromacsParser, "parsers/gromacs/test/examples/water/md.log", "json-events") must_== ParseResult.ParseSuccess
+    }
+    "[tip4p] test with json-events" >> {
+      ParserRun.parse(GromacsParser, "parsers/gromacs/test/examples/tip4p/md.log", "json-events") must_== ParseResult.ParseSuccess
+    }
+    "[aminoacids] test with json" >> {
       ParserRun.parse(GromacsParser, "parsers/gromacs/test/examples/aminoacids/md.log", "json") must_== ParseResult.ParseSuccess
+    }
+    "[argon] test with json" >> {
+      ParserRun.parse(GromacsParser, "parsers/gromacs/test/examples/argon/md.log", "json") must_== ParseResult.ParseSuccess
+    }
+    "[water] test with json" >> {
+      ParserRun.parse(GromacsParser, "parsers/gromacs/test/examples/water/md.log", "json") must_== ParseResult.ParseSuccess
+    }
+    "[tip4p] test with json" >> {
+      ParserRun.parse(GromacsParser, "parsers/gromacs/test/examples/tip4p/md.log", "json") must_== ParseResult.ParseSuccess
     }
   }
 }
