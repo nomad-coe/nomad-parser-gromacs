cfdce2c806262beff140a04fe1b3d66cd86fd091
Alvin Noe Ladines <ladinesalvinnoe@gmail.com>
2021-08-27 00:49:59 +0200

    Adapted further metainfo changes

--- gromacsparser/metainfo/__init__.py
+++ gromacsparser/metainfo/__init__.py
@@ -16,4 +16,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-import gromacsparser.metainfo.gromacs
+from nomad.metainfo import Environment
+
+from . import gromacs
+
+m_env = Environment()
+m_env.m_add_sub_section(Environment.packages, gromacs.m_package)
